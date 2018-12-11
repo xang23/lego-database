eaeed0f5864d51d9a7900eff11849d5440a09619
jennyrudemo <36629887+jennyrudemo@users.noreply.github.com>
2018-12-11 10:40:22 +0100

    Add files via upload

---
+++ searchresult.php
@@ -0,0 +1,16 @@
+<?php include "header.txt";?>
+
+		</div>
+		<div class="main">
+			<!-- php start-->
+			<p>Visar ANTAL resultat för sökordet "SÖKT SATS"...</p>
+			
+			<div id="searchResult">
+			</div>
+			<!-- php slut? -->
+			<div id="searchFilter">
+			</div>
+		
+		</div>
+	</body>
+</html>
---
+++ set.php
@@ -0,0 +1,23 @@
+<?php include "header.txt";?>
+
+		</div>
+		<div class="main">
+			 <div id="setLeftColumn">
+				<div id="setInfo">
+				</div>
+				
+				<div id="availabilityInfo">
+				</div>
+			 </div>
+			 
+			 <div id="setTable">
+				<div id="minifigs">
+				</div>
+				
+				<div id="parts">
+				</div>
+			 </div>
+		
+		</div>
+	</body>
+</html>
