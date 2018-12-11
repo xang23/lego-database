c19322ace35f3182b9e3f27863ab6250db70bec3
jennyrudemo <36629887+jennyrudemo@users.noreply.github.com>
2018-12-11 10:15:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include "header.txt";?>
+
+<!-- Avslutande av header-div -->
+			<h3>Beskrivande text kommer...</h3>
+		</div>
+		
+		<div id="searchBar">
+		<!-- Hela "sökfönstret" -->
+			<div id="searchField">
+			<!-- Söktext in i denna -->
+			</div>
+			<div id="serachButton">
+			</div>
+			
+			<div id="catergorySearch">
+			</div>
+			<div id="yearSearch">
+			</div>
+			<div id="sortBy">
+			</div>
+		
+		</div>
+
+	</body>
+</html>
