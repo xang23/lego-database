5d660bddd59d23ffe9c4da598b88748395555f35
jennyrudemo <36629887+jennyrudemo@users.noreply.github.com>
2018-12-17 16:09:02 +0100

    Update set.php
    Tabeller för både parts och minifigs.
    Visar små bilder i första hand.
    Räknar in multipler av satser i samlingen i totalt antal av en bit som finns i samlingen.

--- set.php
+++ set.php
@@ -12,7 +12,7 @@
 							die ('MySQL connection error.');
 						}
 						//$searchID = $_GET['searchID'];//
-						$searchID = "'3341-1'";
+						$searchID = "'375-2'";
 						
 						$urlBase="http://www.itn.liu.se/~stegu76/img.bricklink.com/";
 
@@ -126,11 +126,11 @@
 								//sökning på collections mm, se bild på Trello
 								//kommer ge antal av biten för varje setID, en rad för varje SetID
 								$bitQuantity = mysqli_query($connection,
-								"SELECT inventory.Quantity, collection.SetID
+								"SELECT inventory.Quantity
 								FROM inventory, collection
 								WHERE inventory.ItemID = '$itemID' AND inventory.ColorID = '$colorID'
 								AND collection.SetID = inventory.SetID
-								LIMIT 5
+								LIMIT 30
 								"
 								);
 								
@@ -140,25 +140,22 @@
 								//while loop för varje rad i sökningen
 								while ($bitRow = mysqli_fetch_array($bitQuantity)){
 									$quantity = $bitRow['Quantity'];
-									$setID = $bitRow['SetID'];
 									
 									//lägga på quantity till en counter
 									$bitCounter += $quantity;
-									
-									//testa om counter >= quant_goal(el counterGoal)
-									//om den når det: lägg countGoal i totPartsCounter
-									if ($bitCounter >= $quantGoal) {
-										$totPartsCounter += $quantGoal;
-										
-										break;
-									}
 								}
 								
 								//när raderna är slut och om counter < quant_goal: return counter
 								//counter eller countGoal ska läggas till i den totala countern (fullCounter)
+								//testa om counter >= quant_goal(el counterGoal)
+									//om den når det: lägg countGoal i totPartsCounter
 								if ($bitCounter < $quantGoal) {
 									$totPartsCounter += $bitCounter;
 								}
+								else if ($bitCounter >= $quantGoal) {
+									$totPartsCounter += $quantGoal;
+								}
+								
 							}
 							
 							//Skriv ut fullCounter i "Varav i samling:"
@@ -184,10 +181,11 @@
 				print("<div id='minifigs'>");
 				
 				$minifigsSearch= mysqli_query($connection,
-				"SELECT minifigs.Minifigname, inventory.ItemID, inventory.Quantity
-				FROM inventory, minifigs
+				"SELECT minifigs.Minifigname, inventory.ItemID, inventory.Quantity,
+				images.has_gif, images.has_jpg, images.has_largegif, images.has_largejpg
+				FROM inventory, minifigs, images
 				WHERE inventory.SetID = $searchID AND inventory.ItemID = minifigs.MinifigID AND
-				inventory.ItemtypeID = 'M'
+				inventory.ItemtypeID = 'M' AND inventory.ItemID=images.ItemID
 				LIMIT 30");
 				
 				print("<table>\n<tr>");
@@ -200,27 +198,153 @@
 				
 				while($minifigRow = mysqli_fetch_array($minifigsSearch)) {
 					$itemID = $minifigRow['ItemID'];
+					$itemtype = 'M';
 					$figName = $minifigRow['Minifigname'];
 					$inventQuant = $minifigRow['Quantity'];
-					$colQuant = 0;
-					//BÖRJA HÄÄÄÄÄÄÄÄÄÄÄÄÄR TJOHO!!!!
-					//Hämta hela bild-url
-					//Räkna ut colQuant
 					
-					print("<td><img src=$imgSource alt=$figName></td>");
+					$bitQuantity = mysqli_query($connection,
+					"SELECT inventory.Quantity AS invQuantity, collection.Quantity AS colQuantity
+					FROM inventory, collection
+					WHERE inventory.ItemID = '$itemID'
+					AND collection.SetID = inventory.SetID
+					LIMIT 30
+					"
+					);
+					
+					
+					//Hämta hela bild-url
+					if($minifigRow['has_gif'] OR $minifigRow['has_largegif']){
+						$filetype = ".gif";
+						
+						if(!$minifigRow['has_gif']){
+							$itemtype = 'ML';
+						};
+					}
+					else{
+						$filetype = ".jpg";
+						
+						if(!$minifigRow['has_jpg']){
+							$itemtype = 'ML';
+						};
+					};
+					
+					$imgsrc = $urlBase.'/'.$itemtype.'/'.$itemID.$filetype;
+					
+					//Räkna ut colQuant (bitCounter)
+					$bitCounter = 0;
+					
+					//while loop för varje rad i sökningen
+					while ($bitRow = mysqli_fetch_array($bitQuantity)){
+						$quantity = $bitRow['invQuantity'] * $bitRow['colQuantity'];
+						 
+						//lägga på quantity till en counter
+						$bitCounter += $quantity;
+					}
+					
+					
+					print("<td><img src=$imgsrc alt=$figName></td>");
 					print ("<td>$figName</td>");
 					print("<td>$itemID</td>");
 					print("<td>$inventQuant</td>");
-					print("<td>$colQuant</td>");
+					print("<td>$bitCounter</td>");
 
 					
-				print ("</tr>\n");
-					
+					print ("</tr>\n");
+				
 				}
 				
 				print("</div>");
 				
+				/***********************
+				*       PARTS          *
+				***********************/
 				print("<div id='parts'>");
+					$partsSearch= mysqli_query($connection,
+					"SELECT parts.Partname, inventory.ItemID, inventory.Quantity, inventory.ColorID,
+					images.has_gif, images.has_jpg, images.has_largegif, images.has_largejpg, colors.Colorname
+					FROM inventory, parts, images, colors
+					WHERE inventory.SetID = $searchID AND inventory.ItemID = parts.PartID AND
+					inventory.ItemtypeID = 'P' AND inventory.ItemID=images.ItemID AND 
+					inventory.ColorID = images.ColorID AND inventory.ColorID = colors.ColorID
+					LIMIT 30");
+					
+					print("<table>\n<tr>");
+					print ("<th> Bild </th>");
+					print ("<th> Namn </th>");
+					print ("<th> FigurID </th>");
+					print ("<th> Färg </th>");
+					print ("<th> Antal i sats </th>");
+					print ("<th> Antal i samling </th>");
+					print ("</tr>\n");
+					
+					//Varje bit
+					while($partsRow = mysqli_fetch_array($partsSearch)) {
+						$itemID = $partsRow['ItemID'];
+						$itemtype = 'P';
+						$partName = $partsRow['Partname'];
+						$colorID = $partsRow['ColorID'];
+						$inventQuant = $partsRow['Quantity'];
+						$colorName = $partsRow['Colorname'];
+						
+						$bitQuantity = mysqli_query($connection,
+						"SELECT inventory.Quantity AS invQuantity, collection.Quantity AS colQuantity
+						FROM inventory, collection
+						WHERE inventory.ItemID = '$itemID' AND inventory.ColorID = $colorID
+						AND collection.SetID = inventory.SetID
+						LIMIT 30"
+						);
+						
+						
+						//Hämta hela bild-url
+						if($partsRow['has_gif'] OR $partsRow['has_largegif']){
+							$filetype = ".gif";
+						
+							if($partsRow['has_gif']){
+								$itemtype = $itemtype."/".$colorID;
+							}
+							else {
+								$itemtype = 'PL';
+							};
+						}
+						else{
+							$filetype = ".jpg";
+							
+							if($partsRow['has_jpg']){
+								$itemtype = $itemtype."/".$colorID;
+							}
+							else {
+								$itemtype = 'PL';
+							};
+						};
+						
+						$imgsrc = $urlBase.'/'.$itemtype.'/'.$itemID.$filetype;
+						
+						//Räkna ut colQuant (bitCounter)
+						$bitCounter = 0;
+						
+						
+						
+						//while loop för varje rad i sökningen
+						while ($bitRow = mysqli_fetch_array($bitQuantity)){
+							
+							//$quantity = $bitRow['invQuantity'] ;
+							$quantity = $bitRow['invQuantity'] * $bitRow['colQuantity'];
+							 
+							//lägga på quantity till en counter
+							$bitCounter += $quantity;
+						}
+						
+						print("<td><img src=$imgsrc alt=$partName></td>");
+						print ("<td>$partName</td>");
+						print("<td>$itemID</td>");
+						print("<td>$colorName</td>");
+						print("<td>$inventQuant</td>");
+						print("<td>$bitCounter</td>");
+
+						
+						print ("</tr>\n");
+					
+					}
 				print("</div>");
 			 print("</div>");
 			?>
