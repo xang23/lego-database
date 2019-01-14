32e3f0716f13f84fc0911f9e811da7d09e0bc151
jennyrudemo <36629887+jennyrudemo@users.noreply.github.com>
2019-01-14 11:09:04 +0100

    Update start.php - dropdown
    Dropwdown för kategorialternativ
    Tillägg i id:n för att ny CSS ska fungera

--- start.php
+++ start.php
@@ -1,31 +1,68 @@
 <?php include "txt/header.txt";?>
 
 <!-- Avslutande av header-div -->
-			<h3>Beskrivande text kommer...</h3>
-		</div>
+		<div id="description">
+      <h3 id="descriptionText" class="headerText">Beskrivande text kommer...</h3>
+    </div>
 
-		<div id="searchBar">
-		<form id="myForm" action ="searchresult.php" method="GET">
-		<!-- Hela "sökfönstret" -->
-			<div id="searchField">
-			<p>Search <input type="text" name="search"/></p>
-			<!-- Söktext in i denna -->
-			</div>
-			<div id="searchButton">
-			<p><input type="submit" /> </p>
-			</div>
+  </div>
+  <div id="background"> 
 
-			<div id="catergorySearch">
-			</div>
-			<div id="yearSearch">
-			</div>
-			<div id="sortBy">
-				<?php
-					include "radio.txt";
-				?>
-			</div>
+		<img id="legobit-test" src="legobit-test.svg" alt="legobit-test">
+
+	  <div id="searchBar">
+			<form id="myForm" action ="searchresult.php" method="GET">
+			  <!-- Hela "sökfönstret" -->
+			  <div id="searchRow1">
+				<input id="searchField" type="text" name="search" placeholder="Sök på en sats här..."/>
+				<!-- Söktext in i denna -->
+			  <!-- <div id="searchButton"> -->
+				<input  id="searchButton" type="submit" />
+			  </div>
+
+			  <div id="catergorySearch">
+				<!--kommer behöva hämta alla kategorier från databasen-->
+				<!--<label for="category">Välj kategori</label>-->
+				<select name="category">
+				  <option value="" disabled selected>Välj kategori...</option>
+				  <option value="starwars">Star Wars</option>
+				  <option value="castle">Castle</option>
+				  <option value="basic">Basic</option>
+				  <option value="duplo">Duplo</option>
+				</select>
+			  </div>
+
+			  <div id="yearSearch">
+				<!--<label for="year">Välj år</label>-->
+				<!--php för att hämta alla år-->
+				<select name="year">
+				  <option value="" disabled selected>Välj år...</option>
+				  <option value="1999">1999</option>
+				  <option value="2000">2000</option>
+				  <option value="2001">2001</option>
+				  <option value="2002">2002</option>
+				</select>
+			  </div>
+
+			  <div id="sortBy">
+				<!--<label for="sortBy">Sortera efter...</label>-->
+				<select name="sortBy">
+				  <option value="" disabled selected>Sortera efter...</option>
+				  <option value="sets">Sats</option>
+				  <option value="parts">Bitar</option>
+				</select>
+				
+						<?php
+							include "radio.txt";
+						?>
+						
+				</div>
+					
 			</form>
 		</div>
-
+		
+		<?php include "txt/footer.txt";?>
+		
+		
 	</body>
 </html>
