b41c62ca6b4077e9a1db01b17acb62f5e7d58c4e
jennyrudemo <36629887+jennyrudemo@users.noreply.github.com>
2019-01-16 11:04:52 +0100

    Update search.php
    Uppdaterad för offset, pagination

--- search.php
+++ search.php
@@ -17,6 +17,7 @@
 		
 		
 		
+		
 			//Spara radiobuttons och söka antingen på setID eller setname
 				if ($_GET["searchorder"] == "sbname")
 				{
@@ -34,22 +35,50 @@
 			echo "rbutton = ".$_COOKIE['rbutton'];
 			if(!$limit = $_GET["limit"])
 			{
-				$limit = 15;
+				$limit = 30;
 			}
 
 			
-			print "GET:". $_GET["searchorder"]. "end";
-		
-		
-		$result = mysqli_query($link, //I FROM behöver vi sets?
-		"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
-		images.has_largegif, sets.Year
-		FROM sets, images 
-		$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
-		ORDER BY $order 
-		LIMIT $limit" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
-		);
-
+			print "GET:". $_GET["searchorder"]. "end ";
+			
+			//Ställer in hur många resultat som visas per sida
+			//flytta upp denna högre upp?
+			$itemsPerPage= 10;
+			
+			if(isset($_GET['page']) && !empty($_GET['page'])){
+				$page = ($_GET['page'] - 1)*$itemsPerPage;
+			}
+			//ev ändra variabel $page till $offset
+			
+			//test
+			print("test ");
+			print("$page");
+			
+		if($page > 0){
+			$result = mysqli_query($link, //I FROM behöver vi sets?
+			"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
+			images.has_largegif, sets.Year
+			FROM sets, images 
+			$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
+			ORDER BY $order 
+			LIMIT $limit OFFSET $page" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
+			);
+			
+			print("HEJ!");
+		}
+		else{
+			$result = mysqli_query($link, //I FROM behöver vi sets?
+			"SELECT sets.SetID, sets.Setname, images.ItemID, images.has_gif, images.has_jpg, images.has_largejpg,
+			images.has_largegif, sets.Year
+			FROM sets, images 
+			$search AND sets.SetID = images.ItemID AND images.ItemtypeID = 'S'
+			ORDER BY $order 
+			LIMIT $limit" //Vi vill ha en offset här < så att man kan visa de andra satserna! Helst med hjälp av att klicka på en knapp elr något.
+			);
+			
+			//test
+			print("else.... ");
+		}
 		print "NEXT:". $_GET["next_page"];
 		print "GET:". $sequered;	
 		print "GET:". $_GET["search"];
